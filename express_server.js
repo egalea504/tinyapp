bcfe4bbf0e75bdc316713314915731e8da623014
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-03-06 19:20:35 -0500

    add urls/id page that displays the short and long URL by id

--- express_server.js
+++ express_server.js
@@ -21,6 +21,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
+// display the long and short URL on the url ID page
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
